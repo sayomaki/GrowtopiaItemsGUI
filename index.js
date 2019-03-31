229cd369d675a3d3c298bf2918f0ee3f687ba28c
willi123yao <willi123yao@gmail.com>
2019-03-31 20:42:42 +0800

    Add table html generator using parsed items

--- index.js
+++ index.js
@@ -80,6 +80,30 @@ const showInfo = () => {
 const browseItems = () => {
     if (!loaded) return;
 
+    let generatedHTML = '';
+    let alt = true;
+    Object.values(parsedDB.items).forEach(item => {
+        if (alt) {
+            generatedHTML += `
+            <tr class="table-primary">
+                <td>${item.itemID}</td>
+                <td>${item.name}</td>
+                <td>Column content</td>
+                <td>Column content</td>
+            </tr>`;
+        }
+        else {
+            generatedHTML += `
+            <tr class="table-secondary">
+                <td>${item.itemID}</td>
+                <td>${item.name}</td>
+                <td>Column content</td>
+                <td>Column content</td>
+            </tr>`;
+        }
+        alt = !alt;
+    });
+
     $('#content').html(`
     <div class="item-content">
         <div class="item-header">
@@ -96,70 +120,11 @@ const browseItems = () => {
                     <th scope="col">Name</th>
                     <th scope="col">Column heading</th>
                     <th scope="col">Column heading</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="table-active">
-                <th scope="row">Active</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                </tr>
-                <tr>
-                <th scope="row">Default</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                </tr>
-                <tr class="table-primary">
-                <th scope="row">Primary</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                </tr>
-                <tr class="table-secondary">
-                <th scope="row">Secondary</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                </tr>
-                <tr class="table-success">
-                <th scope="row">Success</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                </tr>
-                <tr class="table-danger">
-                <th scope="row">Danger</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                </tr>
-                <tr class="table-warning">
-                <th scope="row">Warning</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                </tr>
-                <tr class="table-info">
-                <th scope="row">Info</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                </tr>
-                <tr class="table-light">
-                <th scope="row">Light</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                </tr>
-                <tr class="table-dark">
-                <th scope="row">Dark</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                </tr>
-            </tbody>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${generatedHTML}
+                </tbody>
             </table>
         </div>
     </div>
