18faa2be6d1be2bc75e8e6ef6332f6a0bb3823d7
willi123yao <willi123yao@gmail.com>
2019-03-31 20:26:23 +0800

    Basic index.js with file loading dialog function

---
+++ index.js
@@ -0,0 +1,38 @@
+const app = require('electron').remote; // Load remote compnent that contains the dialog dependency
+const dialog = app.dialog;
+const fs = require('fs');
+
+let itemFile;
+let loaded = false;
+let parsedDB = {};
+let parsedHash = 0;
+
+const getFile = () => {
+    dialog.showOpenDialog((fileNames) => {
+        // fileNames is an array that contains all the selected
+        if(fileNames === undefined){
+            console.log("No file selected");
+            return;
+        }
+
+        if (fileNames.length > 1) {
+            alert("Error: Please only choose 1 file!");
+            return;
+        }
+        else if (fileNames[0].lastIndexOf('.dat') != fileNames[0].length - 4) {
+            alert("Error: Not an items.dat file!");
+            return;
+        }
+
+        fs.readFile(fileNames[0], (err, data) => {
+            if(err){
+            alert("An error ocurred reading the file :" + err.message);
+            return;
+            }
+            
+            itemFile = data;
+            loaded = true;
+            console.log("Loaded items.dat file!");
+        });
+    });
+}
